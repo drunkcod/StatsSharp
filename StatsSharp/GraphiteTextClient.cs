0b5d55e22db9e5daccd79066679393d69b0863f7
Tobbe Gyllebring <tobbe@drunkcod.com>
2016-06-08 18:32:56 +0200

    Extract class to file.

--- StatsSharp/GraphiteTextClient.cs
+++ StatsSharp/GraphiteTextClient.cs
@@ -1,29 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
 
 namespace StatsSharp
 {
-	public struct GraphiteValue
-	{
-		static readonly DateTime UnixExpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-
-		public readonly string Name;
-		public readonly double Value;
-		readonly int unixTime;
-
-		public DateTime TimeStamp => UnixExpoch.AddSeconds(unixTime);
-
-		public GraphiteValue(string name, double value, DateTime timeStamp) {
-			this.Name = name;
-			this.Value = value;
-			this.unixTime = (int)timeStamp.ToUniversalTime().Subtract(UnixExpoch).TotalSeconds;
-		}
-
-		public byte[] GetBytes(Encoding encoding) { return encoding.GetBytes($"{Name} {Value} {unixTime}"); }
-	}
-
 	public class GraphiteTextClient
 	{
 		static readonly byte[] RecordSeparator = { (byte)'\n' };
