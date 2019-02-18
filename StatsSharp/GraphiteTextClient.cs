f9773649d987ad2748d1ccf757e97e958bd5f99f
Tobbe Gyllebring <tobbe@drunkcod.com>
2019-02-18 11:49:17 +0100

    Switched default encoding, made it stats client compatible.

--- StatsSharp/GraphiteTextClient.cs
+++ StatsSharp/GraphiteTextClient.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
@@ -11,23 +11,24 @@ namespace StatsSharp
 		void Send(IEnumerable<GraphiteValue> values);
 	}
 
-	public class GraphiteTextClient : IDisposable, IGraphiteClient
+	public class GraphiteTextClient : IDisposable, IGraphiteClient, IStatsClient
 	{
 		static readonly byte[] RecordSeparator = { (byte)'\n' };
 
 		readonly Socket socket;
+
+		public Encoding Encoding => Encoding.UTF8;
 
 		GraphiteTextClient(string host, int port) {
 			this.socket = new Socket(SocketType.Stream, ProtocolType.IP);
 			socket.Connect(host, port);
 		}
 
-		public static GraphiteTextClient Create(string host, int port = 2003) {
-			return new GraphiteTextClient(host, port);
-		}
+		public static GraphiteTextClient Create(string host, int port = 2003) =>
+			new GraphiteTextClient(host, port);
 
 		public void Send(GraphiteValue value) {
-			socket.Send(value.GetBytes(Encoding.ASCII));
+			socket.Send(value.GetBytes(Encoding));
 			socket.Send(RecordSeparator);
 		}
 
@@ -36,12 +37,20 @@ namespace StatsSharp
 				Send(item);
 		}
 
-		public void Close() {
+		public void Close() =>
 			socket.Close();
-		}
 
-		void IDisposable.Dispose() {
+		void IDisposable.Dispose() =>
 			socket.Dispose();
+
+		public void Send(Metric metric) =>
+			Send(new GraphiteValue(metric.Name, metric.Value.AsDouble(), DateTime.UtcNow));
+
+		public void Send(IEnumerable<Metric> metrics)
+		{
+			var now = DateTime.UtcNow;
+			foreach(var item in metrics)
+				Send(new GraphiteValue(item.Name, item.Value.AsDouble(), now));
 		}
 	}
 }
